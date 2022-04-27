29fad0627f4e4fb5694c445a188b07d945909616
ajnatarajan <ajnatarajan@gmail.com>
2022-04-26 20:00:46 -0700

    light vs. dark mode done

--- CP2/aboutme.js
+++ CP2/aboutme.js
@@ -9,12 +9,25 @@
   }
 
   function toggleMode() {
+    /* Handle body background and text */
     qs("body").classList.toggle("light-body");
     qs("body").classList.toggle("dark-body");
-    const aTags = qsa("nav a");
-    for (let i = 0; i < aTags.length; i++) {
-      aTags[i].classList.toggle("light-text");
-      aTags[i].classList.toggle("dark-text");
+
+    /* Handle nav background */
+    qs("nav").classList.toggle("nav-light");
+    qs("nav").classList.toggle("nav-dark");
+
+    /* Handle nav links */
+    const a_tags = qsa("nav a");
+    for (let i = 0; i < a_tags.length; i++) {
+      a_tags[i].classList.toggle("light-text");
+      a_tags[i].classList.toggle("dark-text");
+    }
+
+    /* Handle nav icons */
+    const nav_icons = qsa("nav img");
+    for (let i = 0; i < nav_icons.length; i++) {
+      nav_icons[i].classList.toggle("hidden");
     }
   }
 
