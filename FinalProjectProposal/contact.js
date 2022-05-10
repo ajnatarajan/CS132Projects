6a3a22bd1a1ad5feb0c66501d4a37c1bbaf2e3d5
ajnatarajan <ajnatarajan@gmail.com>
2022-05-09 23:11:22 -0700

    contact validation finished

--- FinalProjectProposal/contact.js
+++ FinalProjectProposal/contact.js
@@ -17,6 +17,18 @@
    */
   function onSubmit(e) {
     // Will make API Call to handle submission
+    if (id("name").value === "") {
+      alert("Must enter name");
+    } else if (id("email").value === "") {
+      alert("Must enter email");
+    } else if (id("subject").value === "") {
+      alert("Must enter message subject");
+    } else if (id("message-body").value === "") {
+      alert("Must enter message");
+    } else {
+      alert("Message sent");
+      return;
+    }
     e.preventDefault();
   }
 
