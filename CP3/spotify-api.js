d36d4e59a490e406131b2ddf07aa044cc12b44fd
ajnatarajan <ajnatarajan@gmail.com>
2022-05-15 12:19:00 -0700

    JSDoc finished for spotify api js file

--- CP3/spotify-api.js
+++ CP3/spotify-api.js
@@ -1,9 +1,14 @@
 (function () {
   "use strict";
 
-  const clientId = "7bc07b0bb69647c687d4d4d42690ccac";
-  const clientSecret = "d64225d0b2854d8a9850b51f3598478b";
+  const CLIENT_ID = "7bc07b0bb69647c687d4d4d42690ccac";
+  const CLIENT_SECRET = "d64225d0b2854d8a9850b51f3598478b";
 
+  /**
+   * Throw an error if response object is not ok.
+   * @param {Response} response - the response object in question
+   * @returns {Response} response - if response object is ok, returns it
+   */
   function checkStatus(response) {
     if (!response.ok) {
       throw Error("Spotify API Error. Try again later.");
@@ -12,18 +17,28 @@
     }
   }
 
+  /**
+   * Notify the user that the request to the Spotify API has failed.
+   * @param {Error} err - the error object
+   * @returns {void}
+   */
   function handleRequestError(err) {
     let msg = gen("p");
     msg.textContent = err;
     id("spotify-info").appendChild(msg);
   }
 
+  /**
+   * Get access token in order to make calls to Spotify API.
+   * No parameters.
+   * @returns {string} access token
+   */
   async function getToken() {
     let data = await fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+        Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
       },
       body: "grant_type=client_credentials",
     });
@@ -32,6 +47,11 @@
     return data.access_token;
   }
 
+  /**
+   * Get the relevant data for Joel Sunny (my favorite artist)
+   * @param {string} access_token - access token required for Spotify API calls
+   * @returns {JSON} relevant data for my favorite artist
+   */
   async function getFavoriteArtist(access_token) {
     let data = await fetch(
       "https://api.spotify.com/v1/artists/4WC54JUV6ewZOuz8Cl2Cym",
@@ -47,6 +67,11 @@
     return data;
   }
 
+  /**
+   * Get the relevant data for Tyler Herro (my favorite song)
+   * @param {string} access_token - access token required for Spotify API calls
+   * @returns {JSON} relevant data for my favorite song
+   */
   async function getFavoriteSong(access_token) {
     let data = await fetch(
       "https://api.spotify.com/v1/tracks/4DuUwzP4ALMqpquHU0ltAB",
@@ -62,6 +87,11 @@
     return data;
   }
 
+  /**
+   * Get the relevant data for Audrey (my favorite user AKA my girlfriend)
+   * @param {string} access_token - access token required for Spotify API calls
+   * @returns {JSON} relevant data for Audrey
+   */
   async function getFavoriteUser(access_token) {
     let data = await fetch("https://api.spotify.com/v1/users/ahong135", {
       headers: {
@@ -74,6 +104,12 @@
     return data;
   }
 
+  /**
+   * Handle updating the DOM with the data from the Spotify API
+   * @param {string} id - artist, song, or user
+   * @param {*} data - data from Spotify API
+   * @returns {void}
+   */
   function updateSpotifyElement(id, data) {
     let img = gen("img");
     let caption = gen("figcaption");
@@ -89,6 +125,13 @@
     }
   }
 
+  /**
+   * Update the DOM with the proper information for the artist.
+   * @param {Element} img - image element
+   * @param {Element} caption - figure caption element
+   * @param {JSON} data - data from Spotify API
+   * @returns {void}
+   */
   function updateArtist(img, caption, data) {
     img.src = data.images[0].url;
     img.alt = data.name + " Image";
@@ -98,6 +141,13 @@
     });
   }
 
+  /**
+   * Update the DOM with the proper information for the song.
+   * @param {Element} img - image element
+   * @param {Element} caption - figure caption element
+   * @param {JSON} data - data from Spotify API
+   * @returns {void}
+   */
   function updateSong(img, caption, data) {
     img.src = data.album.images[0].url;
     img.alt = data.name + " Image";
@@ -106,6 +156,14 @@
       window.open(`${data.album.external_urls.spotify}`, "_blank");
     });
   }
+
+  /**
+   * Update the DOM with the proper information for the song.
+   * @param {Element} img - image element
+   * @param {Element} caption - figure caption element
+   * @param {JSON} data - data from Spotify API
+   * @returns {void}
+   */
   function updateUser(img, caption, data) {
     img.src = data.images[0].url;
     img.alt = data.display_name + " Image";
@@ -115,6 +173,9 @@
     });
   }
 
+  /**
+   * Initialize access token, make all necessary API calls, update DOM
+   */
   async function init() {
     let ids = ["artist", "song", "user"];
     let promises = [];
@@ -134,5 +195,8 @@
     }
   }
 
+  /* Spec explicitly requires listening for an event so I'm doing this on load
+   * even though this script is already a defer script.
+   */
   window.addEventListener("load", init);
 })();
